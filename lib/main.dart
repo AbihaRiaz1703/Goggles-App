faa95b845a618e8ab160ba8a87ca5972aa7001ee
AbihaRiaz1703 <ramshariaz039@gmail.com>
2024-11-13 23:10:14 +0500

    test comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Fluff());
+  runApp(const MyApp());
 }
 
-class Fluff extends StatefulWidget {
-  const Fluff({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  State<Fluff> createState() => _FluffState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _FluffState extends State<Fluff> {
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.pink.shade100,
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                Image(
-                  image: const AssetImage('images/logo.jpg'),
-                  width: 100,
-                  height: 100,
-                  color: Colors.red.shade100,
-                ),
-                Column(
-                  children: [
-                    Text(
-                      'Goggles',
-                      style: TextStyle(
-                          color: Colors.red.shade100, fontFamily: 'SourGummy'),
-                    )
-                  ],
-                ),
-                const Column(
-                  children: [
-                    Text(
-                      'App',
-                      style:
-                          TextStyle(color: Colors.red, fontFamily: 'SourGummy'),
-                    )
-                  ],
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10, right: 10),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    hintText: 'Your Nickname',
-                    hintStyle: const TextStyle(fontFamily: 'SourGummy'),
-                    fillColor: Colors.blue.shade200,
-                    filled: true),
+    return const MaterialApp(
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: const Color.fromARGB(255, 65, 15, 32),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image(
+                    image: const AssetImage('images/logo.jpg'),
+                    width: 100,
+                    height: 100,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Goggles',
+                        style: TextStyle(
+                            color: Colors.pink, fontFamily: 'SourGummy'),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  const Row(
+                    children: [
+                      Text(
+                        'App',
+                        style: TextStyle(
+                            color: Colors.pink, fontFamily: 'SourGummy'),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 100,
+                  ),
+                  Text('Keep Your Vision Clear For Both Future And World')
+                ],
               ),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                  hintText: 'Email Adress',
-                  hintStyle: const TextStyle(fontFamily: 'SourGummy'),
-                  fillColor: Colors.blue.shade200,
-                  filled: true),
-            ),
-          ],
+              // Padding(
+              //   padding: EdgeInsets.only(left: 10, right: 10),
+              //   child: TextFormField(
+              //     decoration: InputDecoration(
+              //         hintText: 'Your Nickname',
+              //         hintStyle: const TextStyle(fontFamily: 'SourGummy'),
+              //         icon: const Icon(Icons.nature),
+              //         fillColor: Colors.blue.shade200,
+              //         filled: true),
+              //   ),
+              // ),
+              // Padding(
+              //   padding: const EdgeInsets.only(left: 10, right: 10),
+              //   child: TextFormField(
+              //     decoration: InputDecoration(
+              //         hintText: 'Email Adress',
+              //         hintStyle: const TextStyle(fontFamily: 'SourGummy'),
+              //         icon: const Icon(Icons.email_rounded),
+              //         fillColor: Colors.blue.shade200,
+              //         filled: true),
+              //   ),
+              // ),
+            ],
+          ),
         ),
       ),
     );
