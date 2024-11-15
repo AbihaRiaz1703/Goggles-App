86fbae7022ec77e1687167f803838887f54550f7
AbihaRiaz1703 <ramshariaz039@gmail.com>
2024-11-15 18:27:20 +0500

    test comment

--- lib/nain.dart
+++ lib/nain.dart
@@ -15,12 +15,25 @@ class _ProfileState extends State<Profile> {
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 65, 15, 32),
+        title: const Text(
+          'Profile Picture',
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.pink),
+        ),
       ),
       body: const SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Divider(
+              color: Colors.black,
+              thickness: 1,
+            ),
+            VerticalDivider(
+              color: Colors.black,
+              thickness: 1,
+            ),
             CircleAvatar(
               radius: 80,
               backgroundImage: AssetImage('images/pp1'),
@@ -41,6 +54,9 @@ class _ProfileState extends State<Profile> {
               radius: 80,
               backgroundImage: AssetImage('images/pp5'),
             ),
+            Row(
+              children: [Text('Select to pick your AVATAR')],
+            )
           ],
         ),
       ),
