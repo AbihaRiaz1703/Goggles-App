9b281142d94f9194e6bd1402b36335010eae1d27
AbihaRiaz1703 <ramshariaz039@gmail.com>
2024-11-16 16:26:14 +0500

    test comment

--- lib/nain.dart
+++ lib/nain.dart
@@ -8,45 +8,57 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  List<String> _images = [
+    'images/pp3.jfif',
+    'images/pp4.jfif',
+    'images/pp5.jfif'
+  ];
+
+  int selectIndex = -1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal,
+      backgroundColor: Colors.white,
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 65, 15, 32),
-        title: const Text(
+        title: Text(
           'Profile Picture',
           textAlign: TextAlign.center,
           style: TextStyle(color: Colors.pink),
         ),
       ),
-      body: const SafeArea(
+      body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Divider(
-            //   color: Colors.black,
-            //   thickness: 1,
-            // ),
-            // VerticalDivider(
-            //   color: Colors.black,
-            //   thickness: 1,
-            // ),
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('images/pp3.jfif'),
-            ),
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('images/pp4.jfif'),
-            ),
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('images/pp5.jfif'),
+            for (int i = 0; i < _images.length; i++)
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    selectIndex = i;
+                  });
+                },
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      // color: Colors.amber,
+                      shape: BoxShape.circle,
+                      border: i == selectIndex
+                          ? Border.all(color: Colors.green, width: 5)
+                          : Border.all(color: Colors.transparent),
+                      image: DecorationImage(
+                          image: AssetImage('${_images[i]}'),
+                          fit: BoxFit.cover)),
+                ),
+              ),
+            SizedBox(
+              height: 20,
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [Text('Select to pick your AVATAR')],
             )
           ],
